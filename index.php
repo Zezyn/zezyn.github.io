af9886913b28205a6f57df1e00eb03deed1c8b32
Zezyn <7607920511m.h@gmail.com>
2018-07-02 20:25:04 -0700

    new Page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<style>
+    body {
+        color: white;
+        background-color: black;
+    }
+
+    .input {
+        width: 100px;
+        clear: both;
+    }
+
+    .input input {
+        width: 100%;
+        clear: both;
+    }
+</style>
+    <head>
+        <meta charset="UTF-8">
+        <title>Digital Fortress</title>
+    </head>
+    <h1>Digital Fortress</h1>
+    <body>
+        <br>
+        <a href="Media/Lee Fields - Honey Dove.mp3">Lee Fields - Honey Dove</a>
+    </body>
+
+    <footer>
+
+    </footer>
+</html>
